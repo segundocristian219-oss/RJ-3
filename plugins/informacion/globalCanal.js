bc12e7262083399a5ba2c1c368b048f20270fba5
segundocristian219-oss <segundocristian219@gmail.com>
2026-03-13 01:13:44 -0600

    Actualizar globalCanal.js

--- plugins/informacion/globalCanal.js
+++ plugins/informacion/globalCanal.js
@@ -4,7 +4,7 @@ import path from 'path'
 export async function before(m, { conn }) {
   try {
     const nombreBot = global.namebot || '𝐘𝐀𝐈𝐑 𝐁𝐎𝐓'
-    const bannerFinal = 'https://files.catbox.moe/js07dr.jpg'
+    const bannerFinal = 'https://files.catbox.moe/901j7x.jpg'
 
     const canales = [global.idcanal, global.idcanal2].filter(Boolean)
     const newsletterJidRandom = canales.length
