a2ef1e22660f8709c4f7c6fe83a34df9c54f3a0f
segundocristian219-oss <segundocristian219@gmail.com>
2026-04-20 22:05:08 -0600

    Actualizar reglas.js

--- plugins/grupos/reglas.js
+++ plugins/grupos/reglas.js
@@ -1,29 +1,14 @@
-let handler = async (m, { isPrems, conn }) => {
-let time = global.db.data.users[m.sender].lastcofre + 0 // 36000000 10 Horas //86400000 24 Horas
-if (new Date - global.db.data.users[m.sender].lastcofre < 0) throw `[❗𝐈𝐍𝐅𝐎❗] 𝚈𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚂𝚃𝙴 𝚃𝚄 𝙲𝙾𝙵𝚁𝙴\𝚗𝚅𝚄𝙴𝙻𝚅𝙴 𝙴𝙽 *${msToTime(time - new Date())}* 𝙿𝙰𝚁𝙰 𝚅𝙾𝙻𝚅𝙴𝚁 𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚁`
-
-let img = 'https://telegra.ph/file/f49d69baf52c38286c16b.jpg' 
-let texto = `» 𝙍𝙀𝙂𝙇𝘼𝙎 𝙇𝙄𝘿𝙀𝙍𝙀𝙎 𝘽𝘼𝙎𝙄𝘾𝘼𝙎`
-
-const fkontak = {
-	"key": {
-    "participants":"0@s.whatsapp.net",
-		"remoteJid": "status@broadcast",
-		"fromMe": false,
-		"id": "Halo"
-	},
-	"message": {
-		"contactMessage": {
-			"vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
-		}
-	},
-	"participant": "0@s.whatsapp.net"
+let handler = async (m, { conn }) => {
+    let chat = global.db.data.chats[m.chat]
+        if (chat.sRules) {
+            let reglas = chat.sRules;
+            m.reply(reglas);
+        } else {
+            m.reply('𝙀𝙡 𝙜𝙧𝙪𝙥𝙤 𝙣𝙤 𝙩𝙞𝙚𝙣𝙚 𝙧𝙚𝙜𝙡𝙖𝙨');
+        }
 }
-await conn.sendFile(m.chat, img, 'img.jpg', texto, fkontak)
-global.db.data.users[m.sender].lastcofre = new Date * 1
-}
-handler.help = ['reglaslideres']
-handler.tags = ['freefire']
-handler.command = ['reglaslideres'] 
-handler.admin = true
+handler.help = ['reglas']
+handler.tags = ['group']
+handler.command = ['reglas']
+handler.group = true
 export default handler
